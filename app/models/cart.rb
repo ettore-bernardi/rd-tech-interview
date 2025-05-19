5c749e8ff14e43ea61ac6750b6886564d2d5bfa3
ettore-bernardi <ettore.contas@gmail.com>
2025-05-19 22:18:31 +0000

    feat: add status enum to Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,6 +4,8 @@ class Cart < ApplicationRecord
 
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
+  enum status: { active: 0, completed: 1, abandoned: 2 }
+
   def update_total_price
     self.total_price = cart_items.inject(0) { |sum, item| sum + (item.product.price * item.quantity) }
     save
